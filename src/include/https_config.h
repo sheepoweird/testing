08d8e1505a2911dd50d9c269d9a3f8fc1e03929d
piers <piers1231@gmail.com>
2025-11-22 06:18:14 +0800

    Remove unused certs

--- src/include/https_config.h
+++ src/include/https_config.h
@@ -7,42 +7,7 @@
 #define WIFI_SSID "Zzz"
 #define WIFI_PASSWORD "i6b22krm"
 
-// HTTPS Certificate - ISRG Root X1 (Let's Encrypt Root)
-// #define CA_CERT_xxx \
-// "-----BEGIN CERTIFICATE-----\n" \
-// "MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n" \
-// "TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n" \
-// "cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n" \
-// "WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n" \
-// "ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n" \
-// "MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n" \
-// "h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n" \
-// "0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n" \
-// "A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n" \
-// "T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n" \
-// "B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n" \
-// "B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n" \
-// "KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n" \
-// "OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n" \
-// "jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n" \
-// "qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n" \
-// "rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n" \
-// "HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n" \
-// "hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n" \
-// "ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n" \
-// "3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n" \
-// "NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n" \
-// "ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n" \
-// "TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n" \
-// "jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n" \
-// "oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n" \
-// "4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n" \
-// "mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n" \
-// "emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n" \
-// "-----END CERTIFICATE-----\n"
-
-
-// Local mTLS web cert (rename later)
+// Local mTLS web cert
 #define CA_CERT \
 "-----BEGIN CERTIFICATE-----\n" \
 "MIIBdjCCARugAwIBAgIUP2bGak6vBjFYjxl7fBpqRFC8ai8wCgYIKoZIzj0EAwIw\n" \
@@ -66,19 +31,5 @@
 "zoYwCgYIKoZIzj0EAwIDSAAwRQIgdL+GRCSPb7uqIEzB6Wmk/iPwBQlXongLnVOU\n" \
 "XW6XHjMCIQDeOiPCQ6PmR29+zlOsqqp5LXqZ8ReMDzHsKD7tJ+VEHw==\n" \
 "-----END CERTIFICATE-----\n"
-
-
-// #define CLIENT_KEY \
-// "-----BEGIN EC PARAMETERS-----\n" \
-// "BggqhkjOPQMBBw==\n" \
-// "-----END EC PARAMETERS-----\n" \
-// "-----BEGIN EC PRIVATE KEY-----\n" \
-// "MHcCAQEEIEbLG9OnDByUCSa0MwUX/FiedCXJbkJS1znsQV+5DpmroAoGCCqGSM49\n" \
-// "AwEHoUQDQgAEAsL5JKa/H/dJo0OkG40ehmIyENXWUyCK7R8w2Sj1B2a+B4+LGM5B\n" \
-// "fAlpXBFMLuk1EFRyuFBR/MOzUnjUXhXMgQ==\n" \
-// "-----END EC PRIVATE KEY-----\n"
-
-#define CLIENT_KEY_PASS ""
-
 
 #endif /* HTTPS_CONFIG_H */
